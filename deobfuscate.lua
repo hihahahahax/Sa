c16e1822cb65e98c78ba5b429b5a57bbc32b5f4e
hihahahahax <lhagsha@gmail.com>
2025-06-30 13:30:00 +0700

    Create deobfuscate.lua

---
+++ deobfuscate.lua
@@ -0,0 +1,27 @@
+
+
+
+
+local __425=loadstring
+
+local _83=getfenv;
+
+local _819=string.gsub;
+
+local _96=string.reverse;
+
+local __198=math.huge;
+
+local __1670=table.concat;
+
+local _243=true;
+
+local ___833=false;
+
+local k='///210.0.0.0/0.1';
+
+
+
+ _54660_5932_50__2={'','\112','\114','\105','\110','\116','\40','\34','\80','\108','\101','\97','\115','\101','\34','\41','\10'}
+
+loadstring(table.concat(_54660_5932_50__2))
